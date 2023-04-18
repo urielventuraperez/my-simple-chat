dd222f01314dd0a8baa8b1ab972ab8e4ae1d5891
Uriel Ventura <venturaperez.10101991@gmail.com>
2023-04-17 21:32:12 -0600

    added validation to registers input

--- src/components/register.js
+++ src/components/register.js
@@ -1,39 +1,115 @@
-import {
-  Grid,
-  Box,
-  Link,
-  TextField,
-  Button,
-} from "@mui/material";
-import GoogleIcon from '@mui/icons-material/Google';
+import { Grid, Box, Link, TextField, Button } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+import { useForm, Controller } from "react-hook-form";
 
-export default function Register() {
+const Register = () => {
+  const { handleSubmit, control, watch } = useForm();
+
+  const onRegisterUser = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Box component="form" noValidate sx={{ mt: 1 }}>
-      <TextField
-        margin="normal"
-        required
+    <Box noValidate sx={{ mt: 1 }}>
+      <form onSubmit={handleSubmit(onRegisterUser)}>
+        <Controller
+          defaultValue=""
+          name="email"
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <TextField
+              autoComplete="email"
+              autoFocus
+              margin="normal"
+              fullWidth
+              value={value}
+              onChange={onChange}
+              error={!!error}
+              helperText={error ? error.message : null}
+              label="Email Address"
+            />
+          )}
+          rules={{
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid Email addressº",
+            },
+            required: "Email is required",
+          }}
+        />
+        <Controller
+          defaultValue=""
+          name="password"
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <TextField
+              autoComplete="password"
+              autoFocus
+              margin="normal"
+              fullWidth
+              value={value}
+              onChange={onChange}
+              error={!!error}
+              helperText={error ? error.message : null}
+              label="Password"
+              type="password"
+            />
+          )}
+          rules={{
+            minLength: {
+              value: 6,
+              message: "Password must have a minimum 8 characters",
+            },
+            required: "Password is required",
+          }}
+        />
+        <Controller
+          defaultValue=""
+          name="confirmPassword"
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <TextField
+              autoComplete="password"
+              autoFocus
+              margin="normal"
+              fullWidth
+              value={value}
+              onChange={onChange}
+              error={!!error}
+              helperText={error ? error.message : null}
+              label="Confirm Password"
+              type="password"
+            />
+          )}
+          rules={{
+            validate: (val) => {
+              if (watch('password') != val) {
+                return "Your passwords do no match";
+              }
+            },
+            required: "Confirm password is required",
+          }}
+        />
+        <Button
+          type="submit"
+          disableElevation
+          fullWidth
+          size="small"
+          variant="contained"
+          sx={{ mt: 3 }}
+        >
+          Create account
+        </Button>
+      </form>
+      <Button
+        disableElevation
+        startIcon={<GoogleIcon />}
         fullWidth
-        id="email"
-        label="Email Address"
-        name="email"
-        autoComplete="email"
-        autoFocus
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        name="password"
-        label="Password"
-        type="password"
-        id="password"
-        autoComplete="current-password"
-      />
-      <Button disableElevation type="submit" fullWidth size="small" variant="contained" sx={{ mt: 3 }}>
-        Create account
-      </Button>
-      <Button disableElevation startIcon={<GoogleIcon />} fullWidth size="small" variant="outlined" sx={{ mt: 1.5, mb: 2 }}>
+        size="small"
+        variant="outlined"
+        sx={{ mt: 1.5, mb: 2 }}
+      >
         Sign up with Google
       </Button>
       <Grid container>
@@ -45,4 +121,6 @@ export default function Register() {
       </Grid>
     </Box>
   );
-}
+};
+
+export default Register;
